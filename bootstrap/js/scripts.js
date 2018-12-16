d90c46449c70d5165b2dd70612badb99111e3286
Francis Karagu <fkaragu@gmail.com>
2018-12-16 15:30:06 +0300

    add click event for dice1 and dice2

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
     alert("New Game");
   });
 
+  $("#btndice1").click(function(event) {
+    alert("Dice1");
 
+  });
+
+  $("#btndice2").click(function(event) {
+    alert("Dice2");
+  });
+
+  
   });
 
 });
